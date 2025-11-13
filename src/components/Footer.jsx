1dfc1c58a1564f12a8fdc17f7d597bdcc03c09c2
Amarjotsingh1313 <hs354282@gmail.com>
2025-11-13 02:49:20 -0800

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,27 @@
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <div className="Footer px-5 py-2">
+        <div className="row">
+          <div className="col-md-3 align-items-center">
+            <img className="FooterLogo" src="/images/logo.png" alt="Footer Logo" />
+            <p className="pb-3">Your ultimate destination for fast, reliable online shopping. Quality products, competitive prices, lightning-fast delivery.</p>
+            <div className="">
+              
+            </div>
+          </div>
+          <div className="col-md-3">
+
+          </div> 
+           <div className="col-md-3">
+          </div> 
+           <div className="col-md-3">
+          </div>
+        </div>
+
+
+      </div>
+    </>
   )
 }
